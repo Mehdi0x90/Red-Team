fd6df99ce75acf78a0361149b8e825c314afdbf3
Mehdi <pentest.co@gmail.com>
2023-08-28 14:41:42 +0330

    Create Copy_file_and_folder.py

---
+++ Python_Scripts/Copy_file_and_folder.py
@@ -0,0 +1,14 @@
+###################################################
+# Copy file and folder by python
+###################################################
+
+import shutil, os
+
+#copy file (src,dst)
+shutil.copyfile('src_file_path', 'dst_file_path')
+
+#copy folder (src,dst)
+shutil.copytree('src_folder_path', 'dst_folder_path')
+
+#copy file for windows
+os.system('copy src_file_path dst_file_path')
